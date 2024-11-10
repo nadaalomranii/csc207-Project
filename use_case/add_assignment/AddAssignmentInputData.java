d04e6cc4fb2071e0f33810e50449990c9e36b7e8
Nada Alomrani <nadaaalomrani@gmail.com>
2024-11-10 02:43:26 -0500

    Made edits to the Assignment Input Data, Implemented the Data Acess Interface

--- use_case/add_assignment/AddAssignmentInputData.java
+++ use_case/add_assignment/AddAssignmentInputData.java
@@ -8,13 +8,15 @@ public class AddAssignmentInputData {
     private final String name;
     private final String dueDate;
     private final double score;
-    private final double weight
+    private final double weight;
+    private final String courseCode;
 
-    public AddAssignmentInputData(String name, String dueDate, double score, double weight) {
+    public AddAssignmentInputData(String name, String dueDate, double score, double weight, String courseCode) {
         this.name = name;
         this.dueDate = dueDate;
         this.score = score;
         this.weight = weight;
+        this.courseCode = courseCode;
     }
 
     // Getters
@@ -30,4 +32,5 @@ public class AddAssignmentInputData {
     public double getWeight() {
         return weight;
     }
+    public String getCourseCode(){ return courseCode;}
 }

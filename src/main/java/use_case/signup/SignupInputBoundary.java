6dd37b95cd7225953c11de5b1ff1545a75a8fc40
Rara <157529219+Sahirara@users.noreply.github.com>
2024-11-18 12:56:43 -0500

    Implemented SignupInputBoundary

--- src/main/java/use_case/signup/SignupInputBoundary.java
+++ src/main/java/use_case/signup/SignupInputBoundary.java
@@ -1,4 +1,18 @@
 package use_case.signup;
 
+/**
+ * Input Boundary for actions related to signing up.
+ */
 public interface SignupInputBoundary {
+
+    /**
+     * Executes the signup use case.
+     * @param signupInputData the input data
+     */
+    void execute(SignupInputData signupInputData);
+
+    /**
+     * Executes the switch to course list view use case.
+     */
+    void switchToCourseListView();
 }

d87f0f4f7091744b76b65a5319d11fe278edb4da
Rara <157529219+Sahirara@users.noreply.github.com>
2024-11-18 12:16:56 -0500

    Added Signup Interface Adapter files and implemented SignupState

---
+++ src/main/java/interface_adapter/signup/SignupViewModel.java
@@ -0,0 +1,10 @@
+package interface_adapter.signup;
+
+import interface_adapter.ViewModel;
+
+/**
+ * The ViewModel for the user Signup View.
+ */
+public class SignupViewModel extends ViewModel<SignupState> {
+
+}

aa7c0bbc6729ab9b862ffe33107ffa9973d3029b
Rara <157529219+Sahirara@users.noreply.github.com>
2024-11-24 18:08:01 -0500

    Implement SignupInteractor

--- src/main/java/use_case/signup/SignupInteractor.java
+++ src/main/java/use_case/signup/SignupInteractor.java
@@ -1,4 +1,46 @@
 package use_case.signup;
 
-public class SignupInteractor {
+import entity.User;
+import entity.UserFactory;
+
+/**
+ * The signup interactor.
+ */
+public class SignupInteractor implements SignupInputBoundary {
+    private final SignupUserDataAccessInterface userDataAccessObject;
+    private final SignupOutputBoundary userPresenter;
+    private final UserFactory userFactory;
+
+    public SignupInteractor(SignupUserDataAccessInterface userDataAccessObject,
+                            SignupOutputBoundary userPresenter,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.userPresenter = userPresenter;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(SignupInputData signupInputData) {
+        if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
+            userPresenter.prepareFailView("User already exists.");
+        }
+        else if (!signupInputData.getPassword().equals(signupInputData.getConfirmPassword())) {
+            userPresenter.prepareFailView("Passwords do not match.");
+        }
+        else {
+            final User user = userFactory.create(signupInputData.getName(),
+                    signupInputData.getUsername(),
+                    signupInputData.getPassword(),
+                    signupInputData.getEmail());
+            userDataAccessObject.save(user);
+
+            final SignupOutputData signupOutputData = new SignupOutputData(user.getUsername(), false);
+            userPresenter.prepareSuccessView(signupOutputData);
+        }
+    }
+
+    @Override
+    public void switchToCourseListView() {
+        userPresenter.switchToCourseListView();
+    }
 }

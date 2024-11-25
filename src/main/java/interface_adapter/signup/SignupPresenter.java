ea7568633430df14cde06655b3935e40e724a292
Rara <157529219+Sahirara@users.noreply.github.com>
2024-11-24 20:28:07 -0500

    Implement SignupPresenter

--- src/main/java/interface_adapter/signup/SignupPresenter.java
+++ src/main/java/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,48 @@
 package interface_adapter.signup;
 
-public class SignupPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.course_list.CourseListState;
+import interface_adapter.course_list.CourseListViewModel;
+import use_case.signup.SignupOutputBoundary;
+import use_case.signup.SignupOutputData;
+
+/**
+ * The presenter for the signup use case.
+ */
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final SignupViewModel signupViewModel;
+    private final CourseListViewModel courseListViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel,
+                           CourseListViewModel courseListViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.courseListViewModel = courseListViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        final CourseListState courseListState = courseListViewModel.getState();
+        this.courseListViewModel.setState(courseListState);
+        courseListViewModel.firePropertyChanged();
+
+        viewManagerModel.setState(courseListViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        final SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(errorMessage);
+        signupViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void switchToCourseListView() {
+        viewManagerModel.setState(courseListViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
 }

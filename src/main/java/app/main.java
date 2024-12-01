a76409fb59ea55040d1f1199f02c182ab57e9fa3
Nada Alomrani <nadaaalomrani@gmail.com>
2024-11-30 19:33:33 -0500

    Implemented main and appbuilder

--- src/main/java/app/main.java
+++ src/main/java/app/main.java
@@ -3,7 +3,6 @@ package app;
 import javax.swing.JFrame;
 
 public class main {
-
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
@@ -11,10 +10,15 @@ public class main {
                                             .addAddCourseView()
                                             .addCourseListView()
                                             .addAssignmentListView()
+                                            .addSignUpView()
+                                            .addLoginView()
                                             .addCourseEditView()
                                             .addAddAssignmentUseCase()
                                             .addAddCourseUseCase()
                                             .addEditCourseUseCase()
+                                            .addSignUpUseCase()
+                                            .deleteAssignmentUseCase()
+                                            .loginUseCase()
                                             .build();
 
         application.pack();

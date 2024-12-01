20bed3b2ae084f265f4e407b77f70265b494a683
ZarAlisha <zariffalisha9@gmail.com>
2024-11-30 20:45:42 -0500

    exists by name, user and course

--- src/main/java/use_case/add_assignment/AddAssignmentDataAccessInterface.java
+++ src/main/java/use_case/add_assignment/AddAssignmentDataAccessInterface.java
@@ -2,14 +2,17 @@ package use_case.add_assignment;
 
 import entity.Assignment;
 import entity.Course;
+import entity.User;
 
 public interface AddAssignmentDataAccessInterface {
     /**
      * Checks if the course already exists.
      * @param name the assignment name to look for
      * @return true if a course with the given code exists; false otherwise
+     * @param course the course in which to check for the assignment
+     * @param user the user associated with the course and assignment
      */
-    boolean existsByName(String name);
+    boolean existsByName(String name, Course course, User user);
 
-    void saveAssignment(Assignment assignment, Course course);
+    void saveAssignment(Assignment assignment, Course course, User user);
 }
